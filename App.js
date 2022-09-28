ac0f3ff1acdb98085bfc6911cb2bc2e3aacbd50b
Earth_saharat <socool2548@gmail.com>
2022-09-28 11:27:58 +0700

    28-9-22

--- App.js
+++ App.js
@@ -1,41 +1,79 @@
-import { View, Text,ActivityIndicator,FlatList } from 'react-native'
-import React,{useEffect,useState} from 'react'
+import { StyleSheet, Text, View, Button,Image ,SafeAreaView} from "react-native";
+import React from "react";
+import HomeScreen from "./screen/HomeScreen";
+import ProductScreen from "./screen/ProductScreen";
 
-const App = () => {
-  const[isLoading,setLoading]= useState(true);
-  const[data,setData] = useState([]);
+import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import {
+  createDrawerNavigator,
+  DrawerContentScrollView,
+  DrawerItemList,
+  DrawerItem,
+} from "@react-navigation/drawer";
 
-  const getMovie = async ()=>{
-    try{
-      const response = await fetch('https://reactnative.dev/movies.json');
-      const json = await response.json();
-      setData(json.movies)
-    }catch(error){
-      alert(error.message);
-    }finally{
-      setLoading(false);
-    }
-  }
-  
-  useEffect(()=>{
-    getMovie();
-  },[])
+
+const MyTheme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: "rgb(255,45,85)",
+  },
+};
+
+
+
+function CustomDrawerContent(props) {
   return (
-    <View style={{flex:1,padding:20}}>
-      { isLoading ? <ActivityIndicator/> : (
-        <FlatList
-        data={data}
-        keyExtractor ={({id},index)=>id}
-        renderItem={({item})=>(
-          <Text>
-            {item.title}, {item.releaseYear}
-          </Text>
-        )}
+    <SafeAreaView>
+      <Image 
+        style={styles.sideMenuProfileIcon}
+        source={require("./assets/react_logo.png")}
+      />
+      <DrawerContentScrollView {...props}>
+        <DrawerItemList {...props} />
+        <DrawerItem
+          label="Close drawer"
+          onPress={() => props.navigation.closeDrawer()}
         />
-        )
-      }
-    </View>
+      </DrawerContentScrollView>
+    </SafeAreaView>
   );
 }
+const Drawer = createDrawerNavigator();
 
-export default App
+function MyDrawer() {
+  return (
+    <Drawer.Navigator
+      useLegacyImplementation
+      drawerContent={(props) => <CustomDrawerContent {...props} />}
+      screenOptions={{
+        drawerStyle: {
+          backgroundColor: "white",
+          width: 240,
+        },
+      }}
+    >
+      <Drawer.Screen name="Home" component={HomeScreen} />
+      <Drawer.Screen name="Product" component={ProductScreen} />
+    </Drawer.Navigator>
+  );
+}
+const App = () => {
+  return (
+    <NavigationContainer theme={MyTheme}>
+      <MyDrawer />
+    </NavigationContainer>
+  );
+};
+
+const styles = StyleSheet.create({
+  sideMenuProfileIcon: {
+    resizeMode: "center",
+    width: 100,
+    height: 100,
+    borderRadius: 100 / 2,
+    alignSelf: "center",
+  },
+});
+export default App;
